96667a42a7912112f51304070363ec0b0fe91580
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-04-17 23:39:06 +0100

    other thing

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
   return "Hello! I am currently not implemented yet!"
+
+@app.route("/interpret/<hex>")
+def interpret(hex):
+  code = bytes.fromhex(hex)
+  return code
