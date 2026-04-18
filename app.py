e48b95592df9a4238122c4663339b8129ac8611e
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-04-18 16:04:29 +0100

    Some stuff
    still not done

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello! I am currently not implemented yet!"
+  return render_template("interpreter.html")
 
-@app.route("/interpret/<hex>")
-def interpret(hex):
-  code = bytes.fromhex(hex)
-  return code
+@app.route("/interpret/<hex_code>/<hex_input>/<flags>")
+def interpret(hex_code, hex_input, flags):
+  try:
+    code = bytes.fromhex(hex_code)
+    inp = bytes.fromhex(hex_input)
+    flag = int(flags)
+    return code, inp, flags
+  except ValueError:
+    return "ValueError, you didn't specify something correctly!"
