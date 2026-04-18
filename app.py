430ecd0aceea24e6c069d47c19da114be321ba66
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-04-18 17:47:24 +0100

    Summary (required)

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return render_template("interpreter.html")
 
-@app.route("/interpret/<hex_code>/<hex_input>/<flags>")
-def interpret(hex_code, hex_input, flags):
-  try:
-    code = bytes.fromhex(hex_code)
-    inp = bytes.fromhex(hex_input)
-    flag = int(flags)
-    return str((code, inp, flag))
-  except ValueError:
-    return "ValueError, you didn't specify something correctly!"
+@app.route("/interpret/", methods=["POST"])
+def interpret():
+  code = request.form["code"]
+  inp = request.form["input"]
+  flag = request.form["flag"]
+  return str([code, inp, flag])
